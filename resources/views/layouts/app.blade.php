330a24886979562428677b7db90351b50e09aa2c
Ruan Pablo <ruanpablodnzzz@gmail.com>
2024-08-09 10:58:10 -0300

    config livewire, and create the first livewire component

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,11 +12,13 @@
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
         <!-- Scripts -->
+        @livewireStyles
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="font-sans antialiased">
 
                 {{ $slot }}
 
+        @livewireScriptConfig
     </body>
 </html>
